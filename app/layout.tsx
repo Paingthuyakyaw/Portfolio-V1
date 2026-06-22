174b0e3f2d1c6db3d06183cd1dc29e64e2d577ad
Paingthuyakyaw <kyawna2265@gmail.com>
2026-06-22 12:16:48 +0630

    refactor: seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,59 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Paing Thura Kyaw | Full Stack Developer",
+  title: "Paing Thura Kyaw",
+  other: {
+    "google-site-verification": "sanJwsQoeoige0WpxnZbOAiC69i0CLDf6SfMWuzSFCk",
+  },
+  // manifest: "/manifest.json",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.ico", type: "any" },
+    ],
+    apple: "/apple-icon.png",
+  },
   description:
-    "Full Stack Developer specializing in React, Node.js, and React Native.",
+    "A passionate Frontend Developer specializing in modern web technologies like React, Next.js, Node.js and Related Framework. Explore my portfolio to see my latest work.",
+  generator: "Next.js",
+  applicationName: "Portfolio",
+  referrer: "origin-when-cross-origin",
+  // metadataBase: new URL("https://www.rootstudio.asia"),
+  keywords: [
+    "Paing Thura Kyaw",
+    "Paing Thura Kyaw FullStack Developer",
+    "Paing Thura Kyaw Frontend Developer",
+  ],
+  category: "Portfolio",
+  authors: [{ name: "Paing Thura Kyaw" }],
+  creator: "Paing Thura Kyaw",
+  publisher: "Paing Thura Kyaw",
+  openGraph: {
+    title: "Paing Thura Kyaw",
+    description:
+      "A passionate FullStack Developer specializing in modern web technologies like React, Next.js, Node.js and Related Frameworks. Explore my portfolio to see my latest work.",
+    // url: "https://www.rootstudio.asia",
+    siteName: "Paing Thura Kyaw",
+    images: [
+      {
+        url: "",
+        alt: "Paing Thura Kyaw",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Paing Thura Kyaw",
+    description:
+      "A passionate Frontend Developer specializing in modern web technologies like React, Next.js, Node.js and Related Framework. Explore my portfolio to see my latest work.",
+    creator: "Paing Thura Kyaw",
+    images: {
+      url: "",
+      alt: "Paing Thura Kyaw",
+    },
+  },
 };
 
 export default function RootLayout({
